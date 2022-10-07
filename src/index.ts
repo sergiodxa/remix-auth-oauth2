ccf4da1f25d0835cf8ca3f5f7678e5cc1c1d2ef9
Marco De Gaetano <marcodegaetano@me.com>
2022-10-07 17:30:00 +0200

    add type number to id in OAuth2Profile

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ let debug = createDebug("OAuth2Strategy");
 
 export interface OAuth2Profile {
   provider: string;
-  id?: string;
+  id?: string | number;
   displayName?: string;
   name?: {
     familyName?: string;
