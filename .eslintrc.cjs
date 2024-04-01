e141450a30bfce540e52768f07ad26558887b664
Sergio Xalambrí <hello@sergiodxa.com>
2024-04-01 10:54:46 -0500

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,22 @@
-/* eslint-disable unicorn/no-empty-file */
 /* eslint-disable unicorn/prefer-module */
 module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "unicorn"],
-  extends: [
-    "plugin:unicorn/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
-  rules: {
-    "prefer-const": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "no-unused-vars": "off",
-    "no-var": "off",
-    "unicorn/no-null": "off",
-    "unicorn/prefer-node-protocol": "off",
-    "unicorn/filename-case": "off",
-    "unicorn/prevent-abbreviations": "off",
-  },
+	root: true,
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint", "unicorn"],
+	extends: [
+		"plugin:unicorn/recommended",
+		"plugin:@typescript-eslint/recommended",
+		"prettier",
+	],
+	rules: {
+		"prefer-const": "off",
+		"@typescript-eslint/explicit-module-boundary-types": "off",
+		"@typescript-eslint/no-non-null-assertion": "off",
+		"no-unused-vars": "off",
+		"no-var": "off",
+		"unicorn/no-null": "off",
+		"unicorn/prefer-node-protocol": "off",
+		"unicorn/filename-case": "off",
+		"unicorn/prevent-abbreviations": "off",
+	},
 };
