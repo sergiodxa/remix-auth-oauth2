13b3239745cfc41952c9645e441a91ce5e191e39
Sergio Xalambrí <hello@sergiodxa.com>
2024-08-06 00:54:05 -0500

    Update exports script

--- scripts/exports.ts
+++ scripts/exports.ts
@@ -1,49 +1,55 @@
-import { file, spawn } from "bun";
-import { consola } from "consola";
+async function main() {
+	let proc = Bun.spawn([
+		"bunx",
+		"attw",
+		"-f",
+		"table-flipped",
+		"--no-emoji",
+		"--no-color",
+		"--pack",
+	]);
 
-let proc = spawn([
-	"bunx",
-	"attw",
-	"-f",
-	"table-flipped",
-	"--no-emoji",
-	"--no-color",
-	"--pack",
-]);
+	let text = await new Response(proc.stdout).text();
 
-let text = await new Response(proc.stdout).text();
+	let entrypointLines = text
+		.slice(text.indexOf('"remix-i18next/'))
+		.split("\n")
+		.filter(Boolean)
+		.filter((line) => !line.includes("─"))
+		.map((line) =>
+			line
+				.replaceAll(/[^\d "()/A-Za-z│-]/g, "")
+				.replaceAll("90m│39m", "│")
+				.replaceAll(/^│/g, "")
+				.replaceAll(/│$/g, ""),
+		);
 
-let pkg = await file("package.json").json();
+	let pkg = await Bun.file("package.json").json();
+	let entrypoints = entrypointLines.map((entrypointLine) => {
+		let [entrypoint, ...resolutionColumns] = entrypointLine.split("│");
+		if (!entrypoint) throw new Error("Entrypoint not found");
+		if (!resolutionColumns[2]) throw new Error("ESM resolution not found");
+		if (!resolutionColumns[3]) throw new Error("Bundler resolution not found");
+		return {
+			entrypoint: entrypoint.replace(pkg.name, ".").trim(),
+			esm: resolutionColumns[2].trim(),
+			bundler: resolutionColumns[3].trim(),
+		};
+	});
 
-let entrypointLines = text
-	.slice(text.indexOf(`"${pkg.name}/`))
-	.split("\n")
-	.filter(Boolean)
-	.filter((line) => !line.includes("─"))
-	.map((line) =>
-		line
-			.replaceAll(/[^\d "()/A-Za-z│-]/g, "")
-			.replaceAll("90m│39m", "│")
-			.replaceAll(/^│/g, "")
-			.replaceAll(/│$/g, ""),
+	let entrypointsWithProblems = entrypoints.filter(
+		(item) => item.esm.includes("fail") || item.bundler.includes("fail"),
 	);
 
-let entrypoints = entrypointLines.map((entrypointLine) => {
-	let [entrypoint, ...resolutionColumns] = entrypointLine.split("│");
-	return {
-		entrypoint: entrypoint.replace(pkg.name, ".").trim(),
-		esm: resolutionColumns[2].trim(),
-		bundler: resolutionColumns[3].trim(),
-	};
+	if (entrypointsWithProblems.length > 0) {
+		console.error("Entrypoints with problems:");
+		process.exit(1);
+	}
+}
+
+await main().catch((error) => {
+	console.error(error);
+	process.exit(1);
 });
 
-let entrypointsWithProblems = entrypoints.filter(
-	(item) => item.esm.includes("fail") || item.bundler.includes("fail"),
-);
-
-if (entrypointsWithProblems.length > 0) {
-	consola.error("Entrypoints with problems:");
-	process.exit(1);
-} else {
-	consola.success("All entrypoints are valid!");
-}
+export {};
