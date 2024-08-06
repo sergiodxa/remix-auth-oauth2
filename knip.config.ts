e11565f1a1bfa17ce70798f511f89344d2c70b38
Sergio Xalambrí <hello@sergiodxa.com>
2024-08-06 00:40:37 -0500

    Uninstall Knip

--- knip.config.ts
+++
@@ -1,8 +0,0 @@
-import type { KnipConfig } from "knip";
-
-// biome-ignore lint/style/noDefaultExport: Required by tool
-export default {
-	entry: ["./src/index.ts", "./scripts/exports.ts", "./src/index.test.ts"],
-	typescript: true,
-	ignoreDependencies: ["@arethetypeswrong/cli"],
-} satisfies KnipConfig;
